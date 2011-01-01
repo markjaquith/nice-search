972261dd05e9249c4e57036a7d29b78978903486
Mark Jaquith <m@rkj.me>
2011-01-01 21:22:37 +0000

    Version 0.3

--- nice-search.php
+++ nice-search.php
@@ -1,20 +1,18 @@
 <?php
 /*
 Plugin Name: Nice Search
-Version: 0.3-beta-1
-Plugin URI: http://txfx.net/code/wordpress/nice-search/
+Version: 0.3
+Plugin URI: http://txfx.net/wordpress-plugins/nice-search/
 Description: Redirects ?s=query searches to /search/query, and converts %20 to +
 Author: Mark Jaquith
 Author URI: http://coveredwebservices.com/
 */
 
-function txfx_nice_search() {
+function cws_nice_search_redirect() {
 	if ( is_search() && strpos( $_SERVER['REQUEST_URI'], '/wp-admin/' ) === false && strpos( $_SERVER['REQUEST_URI'], '/search/' ) === false ) {
-		wp_redirect( get_bloginfo( 'home' ) . '/search/' . str_replace( ' ', '+', str_replace( '%20', '+', get_query_var( 's' ) ) ) );
+		wp_redirect( home_url( '/search/' . str_replace( array( ' ', '%20' ),  array( '+', '+' ), get_query_var( 's' ) ) ) );
 		exit();
 	}
 }
 
-add_action('template_redirect', 'txfx_nice_search');
-
-?>
+add_action( 'template_redirect', 'cws_nice_search_redirect' );
